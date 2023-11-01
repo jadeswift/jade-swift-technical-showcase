f5e0c5997b3bd90c1791f7146af8df9f9036c145
jadeswift <jswift@private.geonetric.com>
2023-11-01 07:41:43 -0500

    landing page tests fixed, removed msw

--- src/app/landing-page/LandingPage.tsx
+++ src/app/landing-page/LandingPage.tsx
@@ -1,12 +1,12 @@
 import React, {FC} from "react";
 import {Box} from "@mui/material";
-import {ReactComponent as TechnicalShowcase} from "../../assets/technical-showcase-animated.svg";
+import technicalShowcase from "../../assets/technical-showcase-animated.svg";
 import {Link} from "react-router-dom";
 
 export const LandingPage: FC = () => (
     <Box>
         <Link to={"albums"}>
-            <TechnicalShowcase aria-label={'landing-animation'}/>
+            <img src={technicalShowcase} alt="technical showcase" aria-label={'landing-animation'}/>
         </Link>
     </Box>
 );
